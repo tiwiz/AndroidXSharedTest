df7a4b97ceb96a35711f650c8e028785191fb128
Roberto Orgiu <roberto.orgiu@gmail.com>
2019-07-27 21:22:36 -0400

    Create new project

---
+++ openoffline/src/main/java/offline/open/MainActivity.kt
@@ -0,0 +1,12 @@
+package offline.open
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
