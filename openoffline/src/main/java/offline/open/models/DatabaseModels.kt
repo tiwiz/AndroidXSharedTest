a7c21be393797cf2af57bd6c7b4edddf36f4bc14
Roberto Orgiu <roberto.orgiu@gmail.com>
2019-07-27 22:53:56 -0400

    Add Room API

---
+++ openoffline/src/main/java/offline/open/models/DatabaseModels.kt
@@ -0,0 +1,19 @@
+package offline.open.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.threeten.bp.Instant
+
+@Entity(tableName = "articles")
+data class DatabaseModels(
+    @ColumnInfo(name = "article_id") @PrimaryKey val articleId: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "author") val author: String,
+    @ColumnInfo(name = "thumbnail") val thumbnail: String,
+    @ColumnInfo(name = "description") val description: String,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "categories") val categories: List<String>,
+    @ColumnInfo(name = "timestamp") val timestamp: Instant
+)
