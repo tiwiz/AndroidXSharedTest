02d35416d346e33706f80e1cf7c28197329abc43
tiwiz <roberto.orgiu@gmail.com>
2020-09-17 09:57:07 +0200

    Add custom permission example for video

---
+++ app/src/main/java/net/orgiu/tests/constracts/CustomPermissionActivity.kt
@@ -0,0 +1,28 @@
+package net.orgiu.tests.constracts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import net.orgiu.tests.databinding.ActivityContractsBinding
+import net.orgiu.tests.databinding.ActivityCustomPermissionBinding
+
+class CustomPermissionActivity : AppCompatActivity() {
+
+    private val binding: ActivityCustomPermissionBinding by lazy {
+        ActivityCustomPermissionBinding.inflate(layoutInflater)
+    }
+
+    private val openAppSettingsContract =
+        registerForActivityResult(PermissionsSettingsContract()) {
+            Toast.makeText(this, "Came back!", Toast.LENGTH_SHORT).show()
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnOpenSettings.setOnClickListener {
+            openAppSettingsContract.launch(null)
+        }
+    }
+}
